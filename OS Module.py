4291491642e4ac82bd99c98e50ffbfc1359763a7
Nemanja Isajlovic <nisajlovic@domain.local>
2017-08-14 16:46:34 +0200

    Os part, 10min, os.walk method

--- OS Module.py
+++ OS Module.py
@@ -1,1 +1,31 @@
 import os
+from datetime import datetime
+
+# got  current  working dir
+# print (os.getcwd())
+
+# change  directory
+# os.chdir('C://Users//nisajlovic//Documents//Python//Tutoria//')
+
+# make new file
+# os.mkdir('Os-Demo-2')                                       # dubina je 1
+# os.makedirs('Os-Demo-2/Sub-Dir-1-1')            # dubina > 1
+
+
+# remove file
+# os.rmdir('Os-Demo-2')                 # brise samo taj dir
+# os.removedirs('Os-Demo-2/Sub-Dir-1-1')
+
+# rename file
+# os.rename('nesto.txt', 'demo.txt')
+
+
+
+#  print file details
+# print (os.stat('demo.txt').st_mtime)
+ #.st_size                 size of file
+# st_mtime               modification time (time stamp)
+
+
+mod_time = os.stat('demo.txt').st_mtime
+print (datetime.fromtimestamp(mod_time))
